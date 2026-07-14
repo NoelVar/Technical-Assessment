204fed688911434dd8d23b5eeee8eb4ef7feb4f8
NoelVar <varnoel11@gmail.com>
2026-07-14 20:13:35 +0100

    Added new URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const cors = require('cors');
 const app = express()
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://taskmanagementassessment.vercel.app']
+    origin: [
+             'http://localhost:5173', 
+             'https://taskmanagementassessment.vercel.app',
+             'https://taskmanagement-mvr5rf0yh-noelvars-projects.vercel.app'
+            ]
 };
 
 app.use(cors(corsOptions));
